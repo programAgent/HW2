cacd57c16bb237b0835e877e2456b6545120aaeb
Asus <asus@nigger.by>
2025-03-22 16:05:37 +0300

    Внёс исправления в оформлении кода, code-style

--- myprojectExeptions/src/main/java/org/task2/Main.java
+++ myprojectExeptions/src/main/java/org/task2/Main.java
@@ -1,24 +1,20 @@
-package org.task2;
+package main.java.org.task2;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-    public static void callMyExeption() throws MyExeption {
+    public static void callMyException() throws MyException {
         System.out.println("Вызываем ДЗ№2 исключение");
-        throw new MyExeption();
-
+        throw new MyException();
     }
+
     public static void main(String[] args) {
-
         try {
-            callMyExeption();
-        } catch (MyExeption e) {
+            callMyException();
+        } catch (MyException e) {
             System.out.println(e.getMessage());
-
         } finally {
             System.out.println("Выполнение блока finally");
         }
+
         System.out.println("Исключение обработано");
     }
 }
